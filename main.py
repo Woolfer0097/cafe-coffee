aadd0d5de8cdee312a58cae0be4d63135d99be5b
Woolfer0097 <Woolfer0097@yandex.ru>
2020-11-29 12:15:19 +0300

    Изменил отображение данных, теперь показывает тип, сорт и степень прожарки текстом, а не цифрой

--- main.py
+++ main.py
@@ -25,12 +25,22 @@ class CafeCoffee(QMainWindow):
         self.coffee_table.setRowCount(length)
         for i in range(length):
             self.coffee_table.setItem(i, 0, QTableWidgetItem(str(data[i][0])))
-            self.coffee_table.setItem(i, 1, QTableWidgetItem(str(data[i][1])))
-            self.coffee_table.setItem(i, 2, QTableWidgetItem(str(data[i][2])))
-            self.coffee_table.setItem(i, 3, QTableWidgetItem(str(data[i][3])))
+            sql_request = f"SELECT sorts.title FROM coffee LEFT JOIN sorts ON " \
+                          f"sorts.id = coffee.sort_id WHERE sorts.id = {data[i][1]}"
+            sort = str(*[str(*i) for i in cursor.execute(sql_request)])
+            sql_request = f"SELECT degrees.title FROM coffee LEFT JOIN degrees ON " \
+                          f"degrees.id = coffee.degree_roasting_id WHERE degrees.id = " \
+                          f"{data[i][2]}"
+            degree = str(*[str(*i) for i in cursor.execute(sql_request)])
+            sql_request = f"SELECT types.title FROM coffee LEFT JOIN types ON " \
+                          f"types.id = coffee.type_id WHERE types.id = {data[i][3]}"
+            type_coffee = str(*[str(*i) for i in cursor.execute(sql_request)])
+            self.coffee_table.setItem(i, 1, QTableWidgetItem(sort))
+            self.coffee_table.setItem(i, 2, QTableWidgetItem(degree))
+            self.coffee_table.setItem(i, 3, QTableWidgetItem(type_coffee))
             self.coffee_table.setItem(i, 4, QTableWidgetItem(str(data[i][4])))
-            self.coffee_table.setItem(i, 5, QTableWidgetItem(str(data[i][5])))
-            self.coffee_table.setItem(i, 6, QTableWidgetItem(str(data[i][6])))
+            self.coffee_table.setItem(i, 5, QTableWidgetItem(f"{data[i][5]}₽"))
+            self.coffee_table.setItem(i, 6, QTableWidgetItem(f"{data[i][6]}ml"))
 
 
 if __name__ == '__main__':
